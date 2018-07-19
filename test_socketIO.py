9e81759c0ed14984db4338f6fbfa58210f507b9b
jaroslaw-wieczorek <jaroslaw.wieczorek@sealcode.org>
2018-07-19 18:30:09 +0200

    Add requirements

--- test_socketIO.py
+++ test_socketIO.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+@app.route("/")
+def hello():
+	print("HelloWorld!")
+
 if __name__ == '__main__':
     socketio.run(app)
