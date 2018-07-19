1c3150424cc7975fcea78181bb5c4140473cce12
jaroslaw-wieczorek <jaroslaw.wieczorek@sealcode.org>
2018-07-19 18:55:21 +0200

    First fix in setup app

--- test_io.py
+++ test_io.py
@@ -2,12 +2,16 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
 
 @app.route("/")
 def hello():
-	print("HelloWorld!")
+	return "HelloWorld!"
+	
+app.config['SECRET_KEY'] = 'secret!'
+
+socketio = SocketIO(app)
+
+
 
 if __name__ == '__main__':
     socketio.run(app)
