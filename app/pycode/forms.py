c230743308fffb6d547a98b112b27b04d8e11a70
Jarosław <jaroslaw.wieczorek@sealcode.org>
2018-09-29 00:33:43 +0200

    Clean project

--- app/pycode/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import Form
-from wtforms.fields import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired
-
-
-class LoginForm(Form):
-    login = StringField('', validators=[DataRequired()])
-    password = PasswordField('', validators=[DataRequired()])
-    submit = SubmitField('Log in!')
