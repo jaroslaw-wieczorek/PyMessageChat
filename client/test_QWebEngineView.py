a752cb1d4b5988b868414a49ed45e0dbe24fb111
Jarosław <jaroslaw.wieczorek@sealcode.org>
2018-09-29 00:33:26 +0200

    Clean project

--- client/test_QWebEngineView.py
+++ client/test_QWebEngineView.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
@@ -8,7 +9,10 @@ from PyQt5.QtWebEngineWidgets import QWebEngineView
 
 app = QApplication(sys.argv)
 
-url = "https://arkadiusz-wieczorek.pl/"
+url = "localhost:5000"
+
+file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "code/template/login.html"))
+local_url = QUrl.fromLocalFile(file_path)
 
 web = QWebEngineView()
 web.load(QUrl(url))
